7d88ad2c2a5dcb5e803a15ae5250d023cc126d3e
fiba-dev <fibarrola06@gmail.com>
2023-03-27 16:50:47 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ComponenteColor from "./components/container/componenteColor";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<ComponenteColor></ComponenteColor>
+			</header>
+		</div>
+	);
+}
+
+export default App;
